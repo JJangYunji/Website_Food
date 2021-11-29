843bb36f36198d569db0c2e91f68a2be93b982ce
JJangYunji <80668447+JJangYunji@users.noreply.github.com>
2021-11-29 20:35:41 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,11 +8,30 @@ menu.onclick = () => {
   navbar.classList.toggle("active");
 };
 
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
+
 window.onscroll = () => {
   menu.classList.remove(
     "fa-times"
   ); /*toggle은 클래스가 존재한다면 클래스를 제거하고, 클래스가 존재하지 않는다면 클래스를 추가함*/
   navbar.classList.remove("active");
+
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header .navbar a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
 };
 
 document.querySelector("#search-icon").onclick = () => {
@@ -36,3 +55,37 @@ var swiper = new Swiper(".home-slider", {
   },
   loop: true,
 });
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
